fdc73a801bb71caf0c4408e2f849a334a2d107dd
huang yao <5945154+kirakiray@users.noreply.github.com>
2026-01-09 10:24:02 +0800

    feat(hybird-data): 实现混合数据存储系统

---
+++ nos/hybird-data/file.js
@@ -0,0 +1,105 @@
+// 创建存储在单个文件上的会自动保存的数据对象
+// import { Stanz } from "/packages/libs/stanz/main.js";
+let Stanz = $.Stanz;
+
+/**
+ * 更新数据，让右边数据替换左边数据
+ * @param {Object} originData - 原始数据对象
+ * @param {Object} afterData - 新数据对象
+ */
+const updateData = (originData, afterData) => {
+  for (let [key, value] of Object.entries(afterData)) {
+    if (originData[key] === undefined) {
+      // 没有数据就直接塞进去
+      originData[key] = value;
+    } else {
+      if (typeof originData[key] === "object" && typeof value === "object") {
+        updateData(originData[key], value);
+      } else if (originData[key] !== value) {
+        originData[key] = value;
+      }
+    }
+  }
+
+  // 删除旧的数据
+  for (let key of Object.keys(originData)) {
+    if (afterData[key] === undefined) {
+      delete originData[key];
+    }
+  }
+};
+
+// 更新数据
+const refreshData = async (data, handle) => {
+  if (saving[handle.path]) {
+    await saving[handle.path];
+  }
+
+  try {
+    // 先获取内容
+    const content = await handle.text();
+
+    if (!content) {
+      return;
+    }
+
+    updateData(data, JSON.parse(content));
+  } catch (e) {
+    console.error(e, data, handle);
+  }
+};
+
+const saving = {};
+
+// 保存数据到文件
+const saveData = async (data, handle) => {
+  if (saving[handle.path]) {
+    await saving[handle.path];
+  }
+
+  const selfPms = (saving[handle.path] = (async () => {
+    // 判断内容是否已经改变后，确认有改变在执行写入
+    const content = await handle.text();
+    if (content !== JSON.stringify(data)) {
+      // 写入数据
+      await handle.write(JSON.stringify(data));
+    }
+
+    if (selfPms === saving[handle.path]) {
+      saving[handle.path] = null;
+    }
+  })());
+};
+
+export const createSingleData = async ({ handle, disconnect = true }) => {
+  const data = new Stanz({});
+
+  await refreshData(data, handle);
+
+  // 监听数据变动
+  const wid = data.watchTick((e) => {
+    saveData(data, handle);
+  }, 10);
+
+  // 监听文件变动
+  const handleRevoke = await handle.observe(async () => {
+    await refreshData(data, handle);
+  });
+
+  Object.defineProperties(data, {
+    disconnect: {
+      value() {
+        if (disconnect === false) {
+          // 不允许手动撤销监听
+          console.error("Manual revocation of monitoring is not allowed", this);
+          return;
+        }
+        handleRevoke();
+        data.unwatch(wid);
+        data.revoke();
+      },
+    },
+  });
+
+  return data;
+};
---
+++ nos/hybird-data/main.js
@@ -0,0 +1,89 @@
+import { HybirdData } from "./dir/hybird-data.js";
+import { createSingleData } from "./file.js";
+
+export const createData = async (handle, options) => {
+  if (handle.kind === "file") {
+    return createSingleData({ handle });
+  }
+
+  const data = new HybirdData(
+    {},
+    {
+      ...options,
+      handle,
+    }
+  );
+
+  await data.ready();
+
+  return data;
+};
+
+// 直接加载对象数据
+// handle 目标目录handle
+// level 加载几层的数据
+// dataId 目标数据id
+export const loadData = async ({ handle, level = 2, dataId }) => {
+  let finnalData = {};
+
+  if (level <= 0) {
+    Object.defineProperties(finnalData, {
+      _dataId: {
+        value: dataId,
+      },
+      dataStatus: {
+        value: "unload",
+      },
+    });
+
+    return finnalData;
+  }
+
+  // 先加载根数据
+  const dataFileHandle = await handle.get(dataId || "_root");
+
+  const content = await dataFileHandle.text();
+  const data = JSON.parse(content);
+
+  let isArray = true;
+  let maxLength = 0;
+
+  for (let [key, value] of Object.entries(data)) {
+    if (/\D/.test(key)) {
+      isArray = false;
+    } else if (isArray && maxLength < parseInt(key)) {
+      maxLength = parseInt(key);
+    }
+
+    if (/^__dataid__/.test(value)) {
+      const subDataId = value.replace(/^__dataid__/, "");
+
+      // 加载数据
+      const realData = await loadData({
+        handle,
+        level: level - 1,
+        dataId: subDataId,
+      });
+
+      finnalData[key] = realData;
+    } else {
+      finnalData[key] = value;
+    }
+  }
+
+  if (isArray) {
+    finnalData.length = maxLength + 1;
+    finnalData = Array.from(finnalData);
+  }
+
+  Object.defineProperties(finnalData, {
+    _dataId: {
+      value: dataId,
+    },
+    dataStatus: {
+      value: "ok",
+    },
+  });
+
+  return finnalData;
+};
