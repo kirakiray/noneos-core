b2fab9622d95f82bc82c4f3e17dfa61bd8f14927
huang yao <5945154+kirakiray@users.noreply.github.com>
2026-01-22 16:39:36 +0800

    refactor(install): 重构安装模块并拆分功能

--- _install/main.js
+++ _install/main.js
@@ -1,32 +1,25 @@
 import { get, init } from "/nos/fs/handle/main.js";
-import { verifyData } from "/nos/crypto/crypto-verify.js";
 import { unzip } from "/nos/util/zip/main.js";
 import { getFileHash } from "/nos/util/hash/get-file-hash.js";
+import { getOnlineData } from "./online-data.js";
 
-export const install = async () => {
+export const install = async (callback) => {
   // 获取根证书，并生成验证器
-  const rootCert = await fetch("/nos/root-cert.json").then((e) => e.json());
-  const isRootCertValid = await verifyData(rootCert);
-
-  if (!isRootCertValid) {
-    throw new Error("Root certificate verification failed");
-  }
-
-  // 查看在线 nos.json 文件
-  const onlineNosConfig = await fetch("./nos.json").then((res) => res.json());
-
-  // 验证nos.json 中的 hashes 是否被擅改
-  const isNosJsonValid = await verifyData(onlineNosConfig);
-
-  if (!isNosJsonValid) {
-    throw new Error("nos.json verification failed");
-  }
+  const { onlineNosConfig } = await getOnlineData();
 
   // 获取本地 nos.json 文件配置
   await init("nos-config");
-  const configHandle = await get("nos-config/system.json", {
+  let configHandle = await get("nos-config/system.json", {
     create: "file",
   });
+
+  // 判断本地已有数据
+  const configData = await configHandle.json().catch(() => null);
+
+  if (configData && configData.version === onlineNosConfig.version) {
+    // 版本相同，无需重新安装
+    return;
+  }
 
   // 下载zip包
   const zipBlob = await fetch("./nos.zip").then((res) => res.blob());
@@ -64,10 +57,12 @@ export const install = async () => {
     throw new Error(errors.join("\n"));
   }
 
+  const nosMapPath = "nos";
+
   // 写入到本地
   for (let { path, file } of needWriteFiles) {
-    await init("nos");
-    const handle = await get(`nos/${path}`, { create: "file" });
+    await init(nosMapPath);
+    const handle = await get(`${nosMapPath}/${path}`, { create: "file" });
     await handle.write(file);
   }
 
@@ -76,7 +71,7 @@ export const install = async () => {
     JSON.stringify({
       version: onlineNosConfig.version,
       mode: "online",
-      nosMapPath: "nos",
+      nosMapPath,
     }),
   );
 };
