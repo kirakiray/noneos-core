7f64cab8b00afa1e495b00a084978b8af6fffc6a
huang yao <5945154+kirakiray@users.noreply.github.com>
2026-01-23 23:46:26 +0800

    chore: remove the effects of terser

--- rollup.config.js
+++ rollup.config.js
@@ -10,18 +10,26 @@ const pkg = JSON.parse(readFileSync(join(__dirname, "package.json"), "utf-8"));
 
 export default defineConfig({
   input: "sw/src/main.js",
-  output: {
-    file: "sw/dist.js",
-    format: "iife",
-    sourcemap: true,
-    banner: `/* version: ${pkg.version} */`,
-  },
-  plugins: [
-    terser({
-      format: {
-        comments: /version:/,
-      },
-    }),
+  output: [
+    {
+      file: "sw/dist.js",
+      format: "iife",
+      sourcemap: true,
+      banner: `/* noneos-core version: ${pkg.version} */`,
+    },
+    {
+      file: "sw/dist.min.js",
+      format: "iife",
+      sourcemap: true,
+      banner: `/* noneos-core version: ${pkg.version} */`,
+      plugins: [
+        terser({
+          format: {
+            comments: /version:/,
+          },
+        }),
+      ],
+    },
   ],
   watch: {
     include: "sw/src/**",
