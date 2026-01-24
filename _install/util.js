2d4d21503f801a7ef568da3088f1f9507cf47dc1
huang yao <5945154+kirakiray@users.noreply.github.com>
2026-01-24 12:20:28 +0800

    refactor(service-worker): 重构服务 worker 注册逻辑并清理测试文件

---
+++ _install/util.js
@@ -0,0 +1,28 @@
+export const register = async (name) => {
+  const registration = await navigator.serviceWorker.register("/" + name);
+
+  // 等待 Service Worker 成功激活后才返回
+  await navigator.serviceWorker.ready;
+
+  if (registration.active.state !== "activated") {
+    // 监听 Service Worker 激活事件
+    await new Promise((resolve, reject) => {
+      registration.active.addEventListener("statechange", () => {
+        if (registration.active.state === "activated") {
+          console.log("Service Worker activated: ", registration.scope);
+          resolve(registration);
+        }
+      });
+    });
+  }
+
+  return registration;
+};
+
+export const clear = async () => {
+  const registrations = await navigator.serviceWorker.getRegistrations();
+  for (const registration of registrations) {
+    await registration.unregister();
+    console.log("Service Worker unregistered: ", registration.scope);
+  }
+};
