ebcfc4f14760e4dc256ca0b0acff873d86846e47
Yao <kirayao@hotmail.com>
2026-01-25 15:44:48 +0800

    fix(sw): issuse about services worker (#13)
    Co-authored-by: huang yao <5945154+kirakiray@users.noreply.github.com>

--- sw/src/main.js
+++ sw/src/main.js
@@ -13,7 +13,11 @@ const NONEOS_CORE_VERSION = "noneos-core@4.0.10";
 
 self.addEventListener("fetch", (event) => {
   const { request } = event;
-  const { pathname } = new URL(request.url);
+  const { pathname, hostname } = new URL(request.url);
+
+  if (location.hostname !== hostname) {
+    return;
+  }
 
   if (pathname === "/__config") {
     return event.respondWith(reloadSystemConfig());
