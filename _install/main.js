0471845483a8105fbc1dc287ad0d8f70dcf79ca3
Yao <kirayao@hotmail.com>
2026-02-28 16:20:33 +0800

    fix: add delay time in sw worker (#17)
    * Update main.js
    
    * Update nos.json
    
    ---------
    
    Co-authored-by: huang yao <5945154+kirakiray@users.noreply.github.com>

--- _install/main.js
+++ _install/main.js
@@ -78,6 +78,8 @@ export const installServiceWorker = async (callback) => {
 
   const registration = await registerSw("sw.js?v=" + onlineNosConfig.version);
 
+  await new Promise((resolve) => setTimeout(resolve, 500));
+
   return registration;
 };
 
