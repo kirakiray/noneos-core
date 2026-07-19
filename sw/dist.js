e89740e0e6fcacb4d465b98244d1d3226ff50de7
Yao <5945154+kirakiray@users.noreply.github.com>
2026-07-19 15:19:27 +0800

    chore: bump version

--- sw/dist.js
+++ sw/dist.js
@@ -1,4 +1,4 @@
-/* noneos-core version: 4.2.0 */
+/* noneos-core version: 4.2.1 */
 (function () {
   'use strict';
 
@@ -518,7 +518,7 @@
   // let systemConfig = {"version":"4.0.0","mode":"online","nosMapPath":"nos-4.0.0"};
   let systemConfig = {};
 
-  const NONEOS_CORE_VERSION = "noneos-core@4.2.0";
+  const NONEOS_CORE_VERSION = "noneos-core@4.2.1";
 
   self.addEventListener("fetch", (event) => {
     const { request } = event;
--- sw/src/main.js
+++ sw/src/main.js
@@ -12,7 +12,7 @@ import { handleNosToolRequest } from "./modules/nostool-handle.js";
 // let systemConfig = {"version":"4.0.0","mode":"online","nosMapPath":"nos-4.0.0"};
 let systemConfig = {};
 
-const NONEOS_CORE_VERSION = "noneos-core@4.2.0";
+const NONEOS_CORE_VERSION = "noneos-core@4.2.1";
 
 self.addEventListener("fetch", (event) => {
   const { request } = event;
