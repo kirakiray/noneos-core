071288a1195e77f25533ddeccb852c156cf7e4c6
Yao <5945154+kirakiray@users.noreply.github.com>
2026-08-11 20:38:56 +0800

    release: bump version to 4.4.0

--- sw/src/main.js
+++ sw/src/main.js
@@ -20,7 +20,7 @@ import { handleNosToolRequest } from "./modules/nostool-handle.js";
 // let systemConfig = {"version":"4.0.0","mode":"online","nosMapPath":"nos-4.0.0"};
 let systemConfig = {};
 
-const NONEOS_CORE_VERSION = "noneos-core@4.3.2";
+const NONEOS_CORE_VERSION = "noneos-core@4.4.0";
 
 self.addEventListener("fetch", (event) => {
   const { request } = event;
