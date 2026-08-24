f81af1e30e396ecc86e2b8f0fb913d240c45a9df
Yao <5945154+kirakiray@users.noreply.github.com>
2026-08-24 22:27:33 +0800

    chore: bump version

--- sw/dist.js
+++ sw/dist.js
@@ -1,4 +1,4 @@
-/* noneos-core version: 4.4.4 */
+/* noneos-core version: 4.5.1 */
 (function () {
   'use strict';
 
@@ -939,7 +939,7 @@
   // let systemConfig = {"version":"4.0.0","mode":"online","nosMapPath":"nos-4.0.0"};
   let systemConfig = {};
 
-  const NONEOS_CORE_VERSION = "noneos-core@4.4.4";
+  const NONEOS_CORE_VERSION = "noneos-core@4.5.1";
 
   self.addEventListener("fetch", (event) => {
     const { request } = event;
